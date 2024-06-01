fee591fd36e11edd2c4fdfb4208951d042945bbe
Adriaen Limborgh <adriaen95@hotmail.com>
2024-06-01 12:22:54 +0200

    chore: Refactor add_product and remove_product functions in main.py
    Refactor the add_product and remove_product functions in main.py to improve code readability and maintainability. The functions have been split into smaller, more focused functions to handle specific tasks. This will make it easier to understand and modify the code in the future.

--- main.py
+++ main.py
@@ -14,29 +14,30 @@ __human_name__ = "superpy"
 def main():
     # Create an argument parser
     parser = argparse.ArgumentParser(description='Supermarket CLI')
-    
+
     # Add arguments to the parser
-    parser.add_argument('--add-product', nargs=4, metavar=('product_name', 'price', 'quantity', 'expiration_date'), help='Add a new product to the supermarket')
-    parser.add_argument('--remove-product', metavar='product_name', help='Remove a product from the supermarket')
-    parser.add_argument('--list-products', action='store_true', help='List all products in the supermarket')
-    
+    parser.add_argument('-b', '--buy', nargs=4, metavar=('name', 'price', 'quantity', 'expiration'), help='Buy a new product')
+    parser.add_argument('-s', '--sell', metavar='name', help='Sell a product')
+    parser.add_argument('-l', '--list', action='store_true', help='List all products')
+
     # Parse the arguments
     args = parser.parse_args()
 
-    # If the add_product argument is passed, unpack the values and call the add_product function
-    if args.add_product:
-        product_name, price, quantity, expiration_date = args.add_product
-        add_product(product_name, price, quantity, expiration_date)
-    # If the remove_product argument is passed, call the remove_product function
-    elif args.remove_product:
-        remove_product(args.remove_product)
-    # If the list_products argument is passed, call the list_products function
-    elif args.list_products:
+    # If the buy argument is passed, unpack the values and call the buy_product function
+    if args.buy:
+        name, price, quantity, expiration = args.buy
+        buy_product(name, price, quantity, expiration)
+    # If the sell argument is passed, call the sell_product function
+    elif args.sell:
+        sell_product(args.sell)
+    # If the list argument is passed, call the list_products function
+    elif args.list:
         list_products()
     # If no arguments are passed, print the help message
     else:
         parser.print_help()
 
+# Function to add a product to a CSV file
 def add_to_csv(product_name, price, quantity, expiration_date, filename):
     # Open the CSV file in append mode
     with open(filename, 'a', newline='') as file:
@@ -45,11 +46,14 @@ def add_to_csv(product_name, price, quantity, expiration_date, filename):
         writer.writerow([product_name, price, quantity, expiration_date])
     # Print a confirmation message
     print(f'Added product: {product_name} with price: {price} and quantity: {quantity} and expiration date: {expiration_date} to {filename}')
-    
-def add_product(product_name, price, quantity, expiration_date):
+
+# Function to buy a product
+def buy_product(product_name, price, quantity, expiration_date):
+    # Add the product to the 'bought.csv' file
     add_to_csv(product_name, price, quantity, expiration_date, 'bought.csv')
 
-def remove_product(product_name):
+# Function to sell a product
+def sell_product(product_name):
     # Get the product details
     product = get_product_details(product_name)
     if product:
@@ -58,7 +62,11 @@ def remove_product(product_name):
         add_to_csv(product_name, price, quantity, expiration_date, 'sold.csv')
         # Remove the product from the 'bought.csv' file
         remove_from_csv(product_name, 'bought.csv')
+    else:
+        # Print an error message if the product is not found
+        print(f"Error: Product '{product_name}' is out of stock.")
 
+# Function to remove a product from a CSV file
 def remove_from_csv(product_name, filename):
     # Open the CSV file in read mode
     with open(filename, 'r') as file:
@@ -76,8 +84,9 @@ def remove_from_csv(product_name, filename):
         for product in products:
             writer.writerow(product)
         
+# Function to get the details of a product
 def get_product_details(product_name):
-    # Open the CSV file in read mode
+    # Open the 'bought.csv' file in read mode
     with open('bought.csv', 'r') as file:
         reader = csv.reader(file)
         # Read the products into a list
@@ -85,13 +94,15 @@ def get_product_details(product_name):
         # Find the product with the given name
         for product in products:
             if product[0] == product_name:
+                # Return the details of the product
                 return product[1:]
+    # If the product is not found, return None
     return None
     
 # Function to list the products
 def list_products(print_table=True):
     # Open the CSV file in read mode
-    with open('products.csv', 'r') as file:
+    with open('bought.csv', 'r') as file:
         # Create a CSV reader
         reader = csv.reader(file)
         # Read the products into a list
